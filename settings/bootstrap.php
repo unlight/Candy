1bdfdd8ccac55e24aa69f8d4062997c5be89c26c
iamthes <s91630277@gmail.com>
2012-08-26 20:48:06 +0400

    fixed meta robots bug

--- settings/bootstrap.php
+++ settings/bootstrap.php
@@ -8,7 +8,7 @@ if (!function_exists('SetMetaTags')) {
 		if (!$Controller) $Controller = Gdn::Controller();
 		if ($Page->MetaDescription) $Controller->Head->AddTag('meta', array('name' => 'description', 'content' => $Page->MetaDescription, '_sort' => 0));
 		if ($Page->MetaKeywords) $Controller->Head->AddTag('meta', array('name' => 'keywords', 'content' => $Page->MetaKeywords, '_sort' => 0));
-		if ($Page->MetaRobots) $Controller->Head->AddTag('meta', array('name' => 'robots', 'content' => $Page->MetaKeywords, '_sort' => 0));
+		if ($Page->MetaRobots) $Controller->Head->AddTag('meta', array('name' => 'robots', 'content' => $Page->MetaRobots, '_sort' => 0));
 		if ($Page->MetaTitle) $Controller->Head->Title($Page->MetaTitle);
 		$Controller->Head->AddTag('meta', array('http-equiv' => 'content-language', 'content' => Gdn::Locale()->Current()));
 		$Controller->Head->AddTag('meta', array('http-equiv' => 'content-type', 'content' => 'text/html; charset=utf-8'));
